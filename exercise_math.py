52992e8e3c37e01f8ef8c9b7a7bedc6e8d94aab6
Gael2323 <lgherrera@mail.austral.edu.ar>
2025-08-10 16:37:43 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,10 +1,11 @@
 def math():
     a = 57
     b = 7
-    print(a+b)
+    print(a+b) #Operadores +,/,-,*,
     print(a-b)
     print(a*b)
-    print((a+b)/2)
-    print(a//b)
-    print(a%b)
-    print(a/b)
+    print((a+b)/2) 
+    print(a//b) #Para hacer division entera, se debe de poner dos veces el operador de "/"
+    print(a%b) # El operador de % lo que hace es devolver el resto de la division entre los elementos que se ingresan
+    print(a/b) # Hace la division original, con los decimales 
+    # Si se quiere hacer una raiz, la forma mas conveniente seria elevar a 1/2. 2**1/2
